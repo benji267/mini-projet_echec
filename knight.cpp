b4ea1baafd228598e1e6a77a63b57d55c7ddf730
benji267 <benjamin.metzger.mpsi@gmail.com>
2022-03-14 19:23:20 +0100

    mini-projet

--- knight.cpp
+++ knight.cpp
@@ -7,6 +7,12 @@ class Knight : public Piece{
     
     bool est_mouvement_legal(Square origine, Square destination){
         tuple<int,int> origine_coord = origine.convertion();
-        tuple<int,int> destination_coord = destination.convertion();  
+        tuple<int,int> destination_coord = destination.convertion();
+        if(abs(origine.position[1]-destination.position[1])%2==0 || abs(get<0>(destination_coord)-get<0>(origine_coord))%2==0){
+            if(abs(origine.position[1]-destination.position[1])+abs(get<0>(destination_coord)-get<0>(origine_coord))==3){
+                return true;
+            }
+        }
+        return false;  
     }
 };
