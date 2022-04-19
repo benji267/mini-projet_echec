691be54eaec4a580ad3f6088a63016e52eb69a1c
benji267 <benjamin.metzger.mpsi@gmail.com>
2022-04-19 09:31:17 +0200

    avacement thomas dumond fdp

--- README.md
+++ README.md
@@ -22,3 +22,4 @@
 ***
 >**Metzger Benjamin**
 
+
--- piece.h
+++ piece.h
@@ -2,8 +2,6 @@
 #include "square.h"
 #include <iostream>
 #include <string>
-#include <list>
-#include <enum.h>
 
 /**
  * @brief Color est une structe de type enum qui va affecter une valeur
